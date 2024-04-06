f6a95274fa413de1556b78e983e246f7d4741ff8
nasusxd <js8918657@gmail.com>
2024-04-06 16:54:33 -0300

    commit 1

--- viaje.php
+++ viaje.php
@@ -1,10 +1,47 @@
 <?php
-/*La empresa de Transporte de Pasajeros “Viaje Feliz” quiere registrar la información referente a sus viajes. De cada viaje se precisa almacenar el código del mismo, destino, cantidad máxima de pasajeros y los pasajeros del viaje.
+/*La empresa de Transporte de Pasajeros “Viaje Feliz” quiere registrar la información referente
+ a sus viajes. De cada viaje se precisa almacenar el código del mismo, destino, cantidad máxima de pasajeros y los pasajeros del viaje.
 
-Realice la implementación de la clase Viaje e implemente los métodos necesarios para modificar los atributos de dicha clase (incluso los datos de los pasajeros). Utilice clases y arreglos  para   almacenar la información correspondiente a los pasajeros. Cada pasajero guarda  su “nombre”, “apellido” y “numero de documento”.
+Realice la implementación de la clase Viaje e implemente los métodos necesarios para modificar los atributos de dicha clase
+(incluso los datos de los pasajeros). Utilice clases y arreglos  para   almacenar la información correspondiente a los pasajeros.
+ Cada pasajero guarda  su “nombre”, “apellido” y “numero de documento”.
 
-Implementar un script testViaje.php que cree una instancia de la clase Viaje y presente un menú que permita cargar la información del viaje, modificar y ver sus datos.
+Implementar un script testViaje.php que cree una instancia de la clase Viaje y presente un menú que permita cargar la información del viaje,
+ modificar y ver sus datos.
 
-Modificar la clase Viaje para que ahora los pasajeros sean un objeto que tenga los atributos nombre, apellido, numero de documento y teléfono. El viaje ahora contiene una referencia a una colección de objetos de la clase Pasajero. También se desea guardar la información de la persona responsable de realizar el viaje, para ello cree una clase ResponsableV que registre el número de empleado, número de licencia, nombre y apellido. La clase Viaje debe hacer referencia al responsable de realizar el viaje.
+Modificar la clase Viaje para que ahora los pasajeros sean un objeto que tenga los atributos nombre,
+ apellido, numero de documento y teléfono. El viaje ahora contiene una referencia a una colección de objetos de la clase Pasajero.
+  También se desea guardar la información de la persona responsable de realizar el viaje, para ello cree una clase ResponsableV que registre el
+   número de empleado, número de licencia, nombre y apellido. La clase Viaje debe hacer referencia al responsable de realizar el viaje.
 
-Implementar las operaciones que permiten modificar el nombre, apellido y teléfono de un pasajero. Luego implementar la operación que agrega los pasajeros al viaje, solicitando por consola la información de los mismos. Se debe verificar que el pasajero no este cargado mas de una vez en el viaje. De la misma forma cargue la información del responsable del viaje.*
+Implementar las operaciones que permiten modificar el nombre, apellido y teléfono de un pasajero. Luego implementar la 
+operación que agrega los pasajeros al viaje, solicitando por consola la información de los mismos. Se debe verificar que el pasajero 
+no este cargado mas de una vez en el viaje. De la misma forma cargue la información del responsable del viaje.*/ 
+class viaje{
+    private $codigo_viaje;
+    private $destino;
+    private $cant_maxima;
+    private $pasajero
+
+    public function __construct($cod,$dest,$cant,){
+        $this->codigo_viaje=$cod;
+        $this->destino= $dest;
+        $this->cant_maxima= $cant;
+        $this->pasajero=$objPasajero;
+    }
+
+    //gets
+    public function getcodigo_viaje(){
+        return $this->codigo_viaje;
+    }
+    public function getdestino(){
+        return $this->destino;
+    }
+    public function getcant_maxima(){
+        return $this->cant_maxima
+    }
+    public function getpasajero(){
+        $this->pasajero;
+    }
+
+}
